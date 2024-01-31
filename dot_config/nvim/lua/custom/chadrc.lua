4e25189ca4655c29d69f94b4e7a6099185aaa183
jwbrandon <bcasey@jwplayer.com>
2024-01-31 18:19:46 -0500

    Update .config/nvim/lua/custom/chadrc.lua

--- dot_config/nvim/lua/custom/chadrc.lua
+++ dot_config/nvim/lua/custom/chadrc.lua
@@ -12,27 +12,13 @@ M.ui = {
     },
   },
   statusline = {
-    theme = "vscode_colored",
+    theme = "minimal",
     overriden_modules = function(modules)
-      -- print path before file name
-      table.insert(
-        modules,
-        2,
-        (function()
-          local path = vim.api.nvim_buf_get_name(0):match "^.*/"
-          -- replace git root in path with nothing
-          path = path:gsub(vim.fn.finddir(".git", ".;"):gsub('.git', ''), '')
-          -- if there is no git root replace $HOME with ~/
-          path = path:gsub(vim.fn.expand('$HOME/'), '~/')
-          --path = path:gsub(vim.lsp.buf.list_workspace_folders()[1], '<root>')
-          return "%#St_LspStatus# " .. path .. " "
-        end)()
-      )
-
       -- remove branch
-      table.remove(modules, 4)
+      table.remove(modules, 3)
       -- remove utf8
-      table.remove(modules, 10)
+      table.remove(modules, 6)
+
     end,
   }
   --hl_add = {
