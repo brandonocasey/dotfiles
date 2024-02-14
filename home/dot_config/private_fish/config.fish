45241d724d5417b8e52126dd3af0fa8eab31c5b8
= <=>
2024-02-14 12:29:22 -0500

    Update home/.config/fish/config.fish

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -3,7 +3,7 @@ if status is-interactive
     curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update
   end
 
-  if type -q tide && ! set -q -U tide_right_prompt_frame_enabled
+  if type -q tide && ! $tide_right_prompt_frame_enabled
     tide configure --auto --style=Classic --prompt_colors='True color' --classic_prompt_color=Darkest --show_time='12-hour format' --classic_prompt_separators=Slanted --powerline_prompt_heads=Slanted --powerline_prompt_tails=Flat --powerline_prompt_style='Two lines, character' --prompt_connection=Disconnected --powerline_right_prompt_frame=Yes --prompt_connection_andor_frame_color=Darkest --prompt_spacing=Compact --icons='Few icons' --transient=Yes
   end
 
