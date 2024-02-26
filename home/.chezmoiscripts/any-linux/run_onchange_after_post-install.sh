f188f8f4cb58a476d58273afc450aa00cbf4583b
brandonocasey <=>
2024-02-25 23:41:30 -0500

    Update home/.chezmoiscripts/any-linux/post-install.sh

--- home/.chezmoiscripts/any-linux/run_onchange_after_post-install.sh
+++ home/.chezmoiscripts/any-linux/run_onchange_after_post-install.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 export PATH="$PATH:./bin:/opt/homebrew/bin:/usr/local/bin:/usr/local/sbin:/home/linuxbrew/.linuxbrew/bin"
 
+echo "Setting up ssh keys via bitwarden"
 bw login
-bw sync
 mkdir -p ~/.ssh
 cd ~/.ssh
 bw get attachment brandonocasey-github.pub --itemid 0608e72b-a25b-4b7d-ad72-b12101687d21
 bw get attachment brandonocasey-github --itemid 0608e72b-a25b-4b7d-ad72-b12101687d21
 
-chmod 700 ~/.ssh
-chmod 644 ~/.ssh brandonocasey-github.pub
-chmod 600 ~/.ssh brandonocasey-github
+sudo -v
+sudo chmod 700 ~/.ssh
+sudo chmod 644 ~/.ssh brandonocasey-github.pub
+sudo chmod 600 ~/.ssh brandonocasey-github
