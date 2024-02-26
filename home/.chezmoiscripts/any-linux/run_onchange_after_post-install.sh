2e3601928870fd03c79970aa56f8487549f82e0f
brandonocasey <=>
2024-02-25 23:55:18 -0500

    Update home/.chezmoiscripts/any-linux/post-install.sh Add test.bash

--- home/.chezmoiscripts/any-linux/run_onchange_after_post-install.sh
+++ home/.chezmoiscripts/any-linux/run_onchange_after_post-install.sh
@@ -3,12 +3,28 @@ export PATH="$PATH:./bin:/opt/homebrew/bin:/usr/local/bin:/usr/local/sbin:/home/
 
 echo "Setting up ssh keys via bitwarden"
 bw login
-mkdir -p ~/.ssh
-cd ~/.ssh
+export BW_SESSION=$(bw unlock --raw)
+
+TEMP="$(mktemp -d)"
+mkdir -p "$TEMP"
+cd "$TEMP"
+
 bw get attachment brandonocasey-github.pub --itemid 0608e72b-a25b-4b7d-ad72-b12101687d21
 bw get attachment brandonocasey-github --itemid 0608e72b-a25b-4b7d-ad72-b12101687d21
 
 sudo -v
-sudo chmod 700 ~/.ssh
-sudo chmod 644 ~/.ssh brandonocasey-github.pub
-sudo chmod 600 ~/.ssh brandonocasey-github
+mkdir -p ~/.ssh
+
+sudo mv "$TEMP"/* "$HOME/.ssh/"
+sudo chmod 700 "$HOME/.ssh"
+
+sudo find "$HOME/.ssh" -name '*.pub' -print0 |
+    while IFS= read -r -d '' pubkey; do
+      sudo chmod 644 "$pubkey"
+      privkey="${pubkey/.pub/}"
+      if [ -f "$privkey" ]; then
+        sudo chmod 600 "$pubkey"
+      fi
+    done
+
+rm -rf "$TEMP"
