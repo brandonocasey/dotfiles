acec88c2ad6ada3b944744a19cd7c81a79abf7f9
= <=>
2024-02-26 13:38:43 -0500

    Update home/.config/fish/config.fish Update home/.ssh/config

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -73,4 +73,15 @@ if status is-interactive
   bind \co 'scratch'
   # * CTRL-j -> find replace using sad?
 
+  function add_identities -d "Add ssh identities"
+    set -l existing_keys (ssh-add -l)
+    for file in (grep -slR "PRIVATE" $HOME/.ssh/)
+      if not string match -q (string join '' '*' (ssh-keygen -lf "$file") '*') "$existing_keys"
+        ssh-add "$file"
+      end
+    end
+  end
+
+  add_identities
+
 end
