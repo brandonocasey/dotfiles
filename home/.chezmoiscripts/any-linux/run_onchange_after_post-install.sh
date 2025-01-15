03a992158a9fe9f1a2fb85818d72c610fb40ab9d
Brandon Casey <brandonocasey@gmail.com>
2025-01-15 16:12:43 -0500

    Add home/.chezmoiscripts/any-linux/post-install.sh

---
+++ home/.chezmoiscripts/any-linux/run_onchange_after_post-install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if command -v fish 2>/dev/null 1>/dev/null; then
+  echo "Updating fisher plugins"
+  if [ ! -f "$HOME/.config/fish/functions/fisher.fish" ]; then
+    fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update"
+  fi
+  fish -c "fish_update_completions"
+
+fi
