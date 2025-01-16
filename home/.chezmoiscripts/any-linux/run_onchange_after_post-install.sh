24a3831db3f5c1b987a540ff7f3b17cd49f91f69
Brandon Casey <brandonocasey@gmail.com>
2025-01-16 15:22:50 -0500

    Update home/.chezmoiscripts/any-linux/post-install.sh Update home/.config/nvim/lazy-lock.json

--- home/.chezmoiscripts/any-linux/run_onchange_after_post-install.sh
+++ home/.chezmoiscripts/any-linux/run_onchange_after_post-install.sh
@@ -2,6 +2,10 @@
 UNAME="$(uname)"
 export PATH="$PATH:./bin:/opt/homebrew/bin:/usr/local/bin:/usr/local/sbin:/home/linuxbrew/.linuxbrew/bin"
 export MANPATH="$MANPATH:./man:/usr/share/man:/usr/local/man:/usr/local/share/man"
+OUTPUT=/dev/stdout
+if [[ $- == *i* ]]; then
+  OUTPUT=/dev/null
+fi
 cmd_exists() {
   if command -v "$1" 2>/dev/null 1>/dev/null; then
     return 0
@@ -13,25 +17,26 @@ cmd_exists() {
 if cmd_exists mise; then
   echo "installing mise"
   mise install
-  eval "$(~/.local/bin/mise activate bash)"
+  eval "$(mise activate bash)"
 fi
 
 if cmd_exists "fish"; then
-  echo "Updating fisher plugins"
+  echo "Updating fisher plugins and fish manuals"
   if [ ! -f "$HOME/.config/fish/functions/fisher.fish" ]; then
-    fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update" 1>/dev/null
+    fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update" 1>$OUTPUT
   fi
-  fish -c "fish_update_completions" 1>/dev/null
+  fish -c "fish_update_completions" 1>$OUTPUT
 fi
 
 if cmd_exists nvim; then
-  echo "updating nvim"
+  echo "updating nvim plugins / lsp servers"
   if [ ! -d "$HOME/.local/share/nvim/lazy/lazy.nvim" ]; then
-    git clone --filter=blob:none https://github.com/folke/lazy.nvim.git --branch=stable "$HOME/.local/share/nvim/lazy/lazy.nvim" 1>/dev/null
+    git clone --filter=blob:none https://github.com/folke/lazy.nvim.git --branch=stable "$HOME/.local/share/nvim/lazy/lazy.nvim" 1>$OUTPUT
   fi
 
-  nvim --headless "+Lazy! sync" +qa 1>/dev/null
-  nvim --headless "+MasonToolsInstallSync" +qall 1>/dev/null
+  nvim --headless "+Lazy! sync" +qa 1>$OUTPUT
+  nvim --headless "+MasonToolsInstallSync" +qall 1>$OUTPUT
+  nvim --headless "+MasonToolsUpdateSync" +qall 1>$OUTPUT
 fi
 
 if cmd_exists tldr; then
@@ -42,7 +47,8 @@ fi
 
 if cmd_exists chezmoi; then
   if [ ! -f "$HOME/.config/chezmoi/chezmoi.toml" ]; then
-    echo "fixing chezmoi config"
-    chezmoi init
+    echo "writing chezmoi config"
+    FILE="$(chezmoi execute-template < "$(chezmoi source-path)/.chezmoi.toml.tmpl")"
+    echo "$FILE" > "$HOME/.config/chezmoi/chezmoi.toml"
   fi
 fi
