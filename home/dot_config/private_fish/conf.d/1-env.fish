091ffa2a6cf8be90f2b3dfd01b34805c8b9fdd53
Brandon Casey <brandonocasey@gmail.com>
2025-01-17 11:48:23 -0500

    Update home/.config/fish/conf.d/1-env.fish

--- home/dot_config/private_fish/conf.d/1-env.fish
+++ home/dot_config/private_fish/conf.d/1-env.fish
@@ -120,6 +120,8 @@ set -gx KODI_DATA "$XDG_DATA_HOME/kodi"
 set -gx WGETRC "$XDG_CONFIG_HOME/wgetrc"
 set -gx VALE_CONFIG_PATH "$XDG_CONFIG_HOME/vale/.vale.ini"
 set -gx VALE_STYLES_PATH "$XDG_DATA_HOME/vale/styles"
+set -gx AWS_SHARED_CREDENTIALS_FILE "$XDG_CONFIG_HOME/aws/credentials"
+set -gx AWS_CONFIG_FILE "$XDG_CONFIG_HOME/aws/config"
 
 # set -gx GVIMINIT 'let $MYGVIMRC = !has("nvim") ? "$XDG_CONFIG_HOME/vim/gvimrc" : "$XDG_CONFIG_HOME/nvim/init.gvim" | so $MYGVIMRC'
 set -gx VIMINIT 'let $MYVIMRC = !has("nvim") ? "$XDG_CONFIG_HOME/vim/vimrc" : "$XDG_CONFIG_HOME/nvim/init.lua" | so $MYVIMRC'
@@ -138,5 +140,7 @@ set -gx BAT_THEME "TwoDark"
 
 # Don't mangle escaped characters with less
 set -gx LESS "-R"
+
+set -gx _ZO_DATA_DIR "$XDG_DATA_HOME/zoxide"
 
 fish_add_path -a "$HOME/.local/share/npm/bin"
