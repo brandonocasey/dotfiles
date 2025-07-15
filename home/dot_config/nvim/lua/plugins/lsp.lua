f8b801eedbccab438f2b71e75d1cb01f7f37974c
brandonocasey <brandonocasey@gmail.com>
2025-07-14 21:45:35 -0400

    Update home/.config/nvim/lua/plugins/lsp.lua

--- home/dot_config/nvim/lua/plugins/lsp.lua
+++ home/dot_config/nvim/lua/plugins/lsp.lua
@@ -1,7 +1,41 @@
+-- The configuration assumes that you've installed and
+-- enabled the Biome LSP.
+
+-- Whenever an LSP attaches
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+    if not client then
+      return
+    end
+
+    -- When the client is Biome, add an automatic event on
+    -- save that runs Biome's "source.fixAll.biome" code action.
+    -- This takes care of things like JSX props sorting and
+    -- removing unused imports.
+    if client.name == "biome" then
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = vim.api.nvim_create_augroup("BiomeFixAll", { clear = true }),
+        callback = function()
+          vim.lsp.buf.code_action({
+            context = {
+              only = { "source.fixAll.biome" },
+              diagnostics = {},
+            },
+            apply = true,
+          })
+        end,
+      })
+    end
+  end,
+})
 return {
   {
     "mason-org/mason.nvim",
-    opts = { ensure_installed = { "ltex-ls-plus", "gh-actions-language-server", "markdownlint-cli2", "vacuum" } },
+    opts = {
+      ensure_installed = { "ltex-ls-plus", "gh-actions-language-server", "markdownlint-cli2", "vacuum", "biome" },
+    },
   },
 
   {
@@ -11,6 +45,7 @@ return {
 
         marksman = {},
         typos_lsp = {},
+        biome = {},
         docker_compose_language_service = {},
         dockerls = {},
         gh_actions_ls = {},
