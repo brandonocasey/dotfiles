9dfef131c2062bacb1550d02afdec18ad27b5e66
brandonocasey <brandonocasey@gmail.com>
2025-07-19 01:52:07 -0400

    Update home/.config/nvim/lazy-lock.json Update home/.config/nvim/lua/plugins/lsp.lua Update home/.config/nvim/lua/plugins/main.lua Update home/.config/tmux/tmux.conf

--- home/dot_config/nvim/lua/plugins/lsp.lua
+++ home/dot_config/nvim/lua/plugins/lsp.lua
@@ -1,35 +1,3 @@
--- The configuration assumes that you've installed and
--- enabled the Biome LSP.
-
--- Whenever an LSP attaches
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-
-    if not client then
-      return
-    end
-
-    -- When the client is Biome, add an automatic event on
-    -- save that runs Biome's "source.fixAll.biome" code action.
-    -- This takes care of things like JSX props sorting and
-    -- removing unused imports.
-    if client.name == "biome" then
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = vim.api.nvim_create_augroup("BiomeFixAll", { clear = true }),
-        callback = function()
-          vim.lsp.buf.code_action({
-            context = {
-              only = { "source.fixAll.biome" },
-              diagnostics = {},
-            },
-            apply = true,
-          })
-        end,
-      })
-    end
-  end,
-})
 return {
   {
     "mason-org/mason.nvim",
@@ -131,8 +99,7 @@ return {
         },
         biome = {
           settings = {
-            format = true,
-            autoFixOnFormat = true,
+            format = false,
           },
         },
       },
