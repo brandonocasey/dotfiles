2eab02598a9a3f03c2c32889db03f5b75c780d97
Brandon Casey <brandonocasey@gmail.com>
2025-10-08 12:32:04 -0400

    Update .claude/settings.json Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/main.lua

--- home/dot_config/nvim/lua/plugins/main.lua
+++ home/dot_config/nvim/lua/plugins/main.lua
@@ -108,7 +108,7 @@ return {
       skip_confirm_for_simple_edits = true,
     },
     -- Optional dependencies
-    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    dependencies = { { "nvim-mini/mini.icons", opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
